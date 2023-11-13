208032a32d99277a64c969014508e77c4d7370c7
MarcusBigTreeBig <the.marcus.herbert@gmail.com>
2023-11-12 20:54:20 -0800

    question 2 works, should improve though

--- src/question2/Main.java
+++ src/question2/Main.java
@@ -1,27 +1,41 @@
 package question2;
 
+import java.util.ArrayList;
+
 public class Main {
     public static char[] validOperations = {'+', '-', '*', '/'};
 
     public static void main (String[] args) {
-        String expression = "36+24-*7+";
-        int expectedValue = -29;
+        String expression = "3 6 + 2 4 - * 7 +";
+        int expectedValue = -11;
+        System.out.println(valueOfPostfixExpression(expression));
     }
 
-    public static int valueOfPostfixExpression (String expression) {
-        int value = 0;
-        int operand1;
-        int operand2;
-        int result;
-        char[] arr = expression.toCharArray();
-        Stack stack = new Stack();
-        for (char c: arr) {
-            if (isValidOperation(c)) {
+    public static double valueOfPostfixExpression (String expression) {
+        double operand1;
+        double operand2;
+        double result = 0;
+        int start = 0;
+        Stack<Double> stack = new Stack<Double>();
+        ArrayList<String> substrings = new ArrayList<String>();
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == ' ') {
+                if (i != start) {
+                    substrings.add(expression.substring(start, i));
+                }
+                start = i+1;
+            }
+        }
+        substrings.add(expression.substring(start));
+        for (String s: substrings) {
+            if (isNumeric(s)) {
+                stack.push(Double.parseDouble(s));
+            }else if (s.length() == 1 && isValidOperation(s.charAt(0))) {
                 operand2 = stack.top();
                 stack.pop();
                 operand1 = stack.top();
                 stack.pop();
-                switch (c) {
+                switch (s.charAt(0)) {
                     case '+':
                         result = operand1+operand2;
                         break;
@@ -35,12 +49,10 @@ public class Main {
                         result = operand1/operand2;
                         break;
                 }
-                stack.push(result);
-            }else{
-                stack.push(c);
+                stack.push(Double.valueOf(result));
             }
         }
-        return value;
+        return stack.top();
     }
 
     public static boolean isValidOperation (char c) {
@@ -50,5 +62,17 @@ public class Main {
             }
         }
         return false;
+    }
+
+    public static boolean isNumeric(String s) {
+        if (s == null) {
+            return false;
+        }
+        try {
+            Double.parseDouble(s);
+            return true;
+        } catch (NumberFormatException exception) {
+            return false;
+        }
     }
 }
